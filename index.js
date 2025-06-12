9c9ad19c413029db2ef7618aa7760a3628290ca7
colman nicolas <colman.nicolas.17@hotmail.com>
2025-06-12 10:22:10 -0300

    se cambia puerto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
